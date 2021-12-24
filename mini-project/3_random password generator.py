7af32bde2ed3d832d3fc9b151ca5f0208c95f349
amshuDhungel <dhungelamshu05@gmail.com>
2021-12-24 17:34:15 +0545

    guess the number

--- mini-project/3_random password generator.py
+++ mini-project/3_random password generator.py
@@ -1,5 +1,25 @@
-import random
 import string
+import random
 
+
+if __name__ == "__main__":
+    s1=string.ascii_letters
+    #print(s1)
+    s2=string.ascii_lowercase
+    #print(s2)
+    s3=string.digits
+    #print(s3)
+    s4=string.punctuation
+    #print(s4)
+passlen= int(input("enter the length of password: "))
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+random.shuffle(s)
+print("Your password is: ")
+print("".join(s[0:passlen]))
+    
 
 
